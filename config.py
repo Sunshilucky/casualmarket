f4795b8b6970857f7c523b91b4629ae93550ffa2
Sunshilucky <lasopas13@gmail.com>
2026-06-05 19:26:55 -0600

    conexion clever cloud

--- config.py
+++ config.py
@@ -3,3 +3,11 @@ class Config:
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'programacion'
     MYSQL_DB = 'tienda_merch'
+    
+db_config = {
+    "host": "bxlz9bzwricjw6zwlfuh-mysql.services.clever-cloud.com",
+    "user": "uytyzzxkyc4xihoz",
+    "password": "SS7sJLDgfQPbBo2ltHEW",
+    "database": "bxlz9bzwricjw6zwlfuh",
+    "port": 3306
+}
